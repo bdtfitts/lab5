e67f7603478eb2a3212a4b2c0363d91e3067311e
Braxton Fitts <bfitts@ucsd.edu>
2016-02-04 12:35:31 -0800

    Lab5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,16 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
+exports.addFriend = function(req, res) {
 	// Your code goes here
- }
+	var name = req.query.name;
+	var description = req.query.description;
+	var imageURL = "http://www.lorempixel.com/500/500/people"
+	var person = {
+		'name': name,
+		'description': description,
+		'imageURL': imageURL
+	}
+	console.log(person);
+	data["friends"].push(person);
+	res.render('add');
+}
